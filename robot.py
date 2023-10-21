4de533850b012489ea2c0232a7e3db9b53e9d1f3
Stephen <stephencferro@gmail.com>
2023-10-20 22:05:55 -0500

    Updated with a* working on large grid, small grid needs debugging.

--- robot.py
+++ robot.py
@@ -8,20 +8,75 @@ class Robot:
         self.yRange = [yMin, yMax]
         self.xLen = int((xMax - xMin) / gridSize - 1)
         self.yLen = int((yMax - yMin) / gridSize - 1)
-        self.map = np.full((self.xLen, self.yLen), False)
         self.landmarkGT = {}
         self.start = [0,0]
         self.goal = [0,0]
-        self.openSet = {}
-        self.explored = {}
+        self.openSet = []
+        self.occupiedCells = []
+        self.f_list = []
+        self.g_list = []
+        self.stepCost = 1
+        self.obsCost = 1000
+        self.pathX = []
+        self.pathY = []
 
 
     def a_star(self, start, goal):
         self.start = start
         self.goal = goal
-        self.openSet = {start: 0}
-        while openSet 
-
+        self.openSet.append(start)
+        self.f_list.append(0)
+        self.g_list.append(0)
+        atGoal = False
+        indexPoint = 0
+        while atGoal == False:
+            indexPoint = self.f_list.index(min(self.f_list))
+            point = self.openSet[indexPoint]
+            print(min(self.f_list))
+            print(point)
+            neighborsX = [point[0] - self.gridSize, point[0], point[0] + self.gridSize]
+            neighborsY = [point[1] - self.gridSize, point[1], point[1] + self.gridSize]
+            for xPos in neighborsX:
+                for yPos in neighborsY:
+                    stepCost = 1
+                    if self.goal == [xPos, yPos]:
+                        atGoal = True
+                        self.openSet.append([xPos,yPos])
+                        self.f_list.append(0)
+                        self.g_list.append(g)
+                    elif (xPos == point[0]) and (yPos == point[1]):
+                        pass
+                    else:
+                        try:
+                            index = self.occupiedCells.index([xPos, yPos])
+                            stepCost = 1000
+                        except ValueError as e:
+                            pass
+                        if (xPos < self.xRange[0]) or (xPos > self.xRange[1]) or (yPos < self.yRange[0]) or (yPos > self.yRange[1]):
+                            stepCost = 1000
+                        h = ((goal[0] - xPos)**2 + (goal[1] - yPos)**2)**0.5   # Hueristic Equation, h = sqrt(x**2 + y**2)
+                        g = self.g_list[indexPoint] + stepCost
+                        f = g + h
+                        try:
+                            indexF = self.openSet.index([xPos,yPos])
+                            self.openSet.pop(indexF)
+                            self.f_list.pop(indexF)
+                            self.g_list.pop(indexF)
+                        except ValueError as e:
+                            pass
+                        self.openSet.append([xPos,yPos])
+                        self.f_list.append(f)
+                        self.g_list.append(g)
+                        print([xPos, yPos, f, g, h])
+            print('cycle')
+            self.openSet.pop(indexPoint)
+            self.f_list.pop(indexPoint)
+            self.g_list.pop(indexPoint)
+            self.pathX.append(point[0])
+            self.pathY.append(point[1])
+        self.pathX.append(self.goal[0])
+        self.pathY.append(self.goal[1])
+      
 
     def import_landmark_GT(self, filename): 
         # Imports data from specified file. File should be in same directory as the python files
@@ -37,12 +92,15 @@ class Robot:
         self.landmarkGT = newData
         return newData
 
+
     def mark_occupied_cells(self):
         # Marks cells that are occupied by landmarks
         for landmark in self.landmarkGT:
-            xIndex = np.floor(landmark[0] * self.gridSize)
-            yIndex = np.floor(landmark[1] * self.gridSize)
-            self.map[xIndex][yIndex] = True
+            xPos = (np.floor(self.landmarkGT[landmark][0] / self.gridSize) + 0.5) * self.gridSize
+            yPos = (np.floor(self.landmarkGT[landmark][1] / self.gridSize) + 0.5) * self.gridSize
+            self.occupiedCells.append([xPos,yPos])
+        print(self.occupiedCells)
+
 
     def draw_map(self, ax):
         # function to draw the map and show the robot's path
@@ -57,10 +115,31 @@ class Robot:
                 yPosGT.append(self.start[1])
         ax.plot(xPosGT, yPosGT, 'k-', label='Robot Ground Truth Position')
 
+
     def plot_landmark_pos(self, ax):
         # Plots the locations on the landmarks
         for pos in self.landmarkGT:
             xPos = np.floor(float(self.landmarkGT[pos][0]) / self.gridSize) * self.gridSize
             yPos = np.floor(float(self.landmarkGT[pos][1]) / self.gridSize) * self.gridSize
-            print([xPos,yPos])
             ax.add_patch(patches.Rectangle([xPos, yPos], self.gridSize, self.gridSize, fill=True, color='k'))
+
+
+    def plot_start_goal(self, ax):
+        # Plots the locations on the start and goal cell
+        startX = self.start[0] - (0.5 * self.gridSize)
+        startY = self.start[1] - (0.5 * self.gridSize)
+        goalX = self.goal[0] - (0.5 * self.gridSize)
+        goalY = self.goal[1] - (0.5 * self.gridSize)
+        ax.add_patch(patches.Rectangle([startX, startY], self.gridSize, self.gridSize, fill=True, color='g'))
+        ax.add_patch(patches.Rectangle([goalX, goalY], self.gridSize, self.gridSize, fill=True, color='b'))
+        ax.plot(self.pathX, self.pathY, 'r-', label='Robot Path')
+
+
+    def reset_robot_path_data(self):
+        self.start = [0,0]
+        self.goal = [0,0]
+        self.openSet = []
+        self.f_list = []
+        self.g_list = []
+        self.pathX = []
+        self.pathY = []
--- run.py
+++ run.py
@@ -7,26 +7,195 @@ xMin = -2
 xMax = 5
 yMin = -6
 yMax = 6
-gridSize = 0.5
+gridSizeA = 1
+gridSizeB = 0.1
 filenameLandmarks = "ds0_Landmark_Groundtruth.dat"
 
-robot = Robot(gridSize, xMin, xMax, yMin, yMax)
+robot_large_grid = Robot(gridSizeA, xMin, xMax, yMin, yMax)
+robot_large_grid.import_landmark_GT(filenameLandmarks)
+robot_large_grid.mark_occupied_cells()
 
-robot.import_landmark_GT(filenameLandmarks)
+# QUESTION 3
 
+# CASE A
+robot_large_grid.a_star([0.5, -1.5], [0.5, 1.5])
 # Create plot
 fig, ax = plt.subplots(figsize=(8, 8))
 plt.xlim(xMin, xMax)
 plt.ylim(yMin, yMax)
 plt.grid(which='minor')
 plt.grid(which='major')
-robot.draw_map(ax)
-robot.plot_landmark_pos(ax)
+robot_large_grid.plot_landmark_pos(ax)
+robot_large_grid.plot_start_goal(ax)
 ax.set_aspect(1)
-ax.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Dead Reckoning Position, Test Data (Question 2)')
+ax.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case A (Question 3)')
 ax.xaxis.set_major_locator(MultipleLocator(1))
 ax.yaxis.set_major_locator(MultipleLocator(1))
-ax.xaxis.set_minor_locator(MultipleLocator(gridSize))
-ax.yaxis.set_minor_locator(MultipleLocator(gridSize))
+ax.xaxis.set_minor_locator(MultipleLocator(gridSizeA))
+ax.yaxis.set_minor_locator(MultipleLocator(gridSizeA))
 ax.legend()
+
+# CASE B
+robot_large_grid.reset_robot_path_data()
+robot_large_grid.a_star([4.5, 3.5], [4.5, -1.5])
+# Create plot
+fig, bx = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_large_grid.plot_landmark_pos(bx)
+robot_large_grid.plot_start_goal(bx)
+bx.set_aspect(1)
+bx.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case B (Question 3)')
+bx.xaxis.set_major_locator(MultipleLocator(1))
+bx.yaxis.set_major_locator(MultipleLocator(1))
+bx.xaxis.set_minor_locator(MultipleLocator(gridSizeA))
+bx.yaxis.set_minor_locator(MultipleLocator(gridSizeA))
+bx.legend()
+
+# CASE C
+robot_large_grid.reset_robot_path_data()
+robot_large_grid.a_star([-0.5, 5.5], [1.5, -3.5])
+# Create plot
+fig, cx = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_large_grid.plot_landmark_pos(cx)
+robot_large_grid.plot_start_goal(cx)
+cx.set_aspect(1)
+cx.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case C (Question 3)')
+cx.xaxis.set_major_locator(MultipleLocator(1))
+cx.yaxis.set_major_locator(MultipleLocator(1))
+cx.xaxis.set_minor_locator(MultipleLocator(gridSizeA))
+cx.yaxis.set_minor_locator(MultipleLocator(gridSizeA))
+cx.legend()
+
+
+# QUESTION 5
+
+# CASE A
+robot_large_grid.reset_robot_path_data()
+robot_large_grid.a_star([0.5, -1.5], [0.5, 1.5])
+# Create plot
+fig, dx = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_large_grid.plot_landmark_pos(dx)
+robot_large_grid.plot_start_goal(dx)
+dx.set_aspect(1)
+dx.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case A (Question 5)')
+dx.xaxis.set_major_locator(MultipleLocator(1))
+dx.yaxis.set_major_locator(MultipleLocator(1))
+dx.xaxis.set_minor_locator(MultipleLocator(gridSizeA))
+dx.yaxis.set_minor_locator(MultipleLocator(gridSizeA))
+dx.legend()
+
+# CASE B
+robot_large_grid.reset_robot_path_data()
+robot_large_grid.a_star([4.5, 3.5], [4.5, -1.5])
+# Create plot
+fig, ex = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_large_grid.plot_landmark_pos(ex)
+robot_large_grid.plot_start_goal(ex)
+ex.set_aspect(1)
+ex.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case B (Question 5)')
+ex.xaxis.set_major_locator(MultipleLocator(1))
+ex.yaxis.set_major_locator(MultipleLocator(1))
+ex.xaxis.set_minor_locator(MultipleLocator(gridSizeA))
+ex.yaxis.set_minor_locator(MultipleLocator(gridSizeA))
+ex.legend()
+
+# CASE C
+robot_large_grid.reset_robot_path_data()
+robot_large_grid.a_star([-0.5, 5.5], [1.5, -3.5])
+# Create plot
+fig, fx = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_large_grid.plot_landmark_pos(fx)
+robot_large_grid.plot_start_goal(fx)
+fx.set_aspect(1)
+fx.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case C (Question 5)')
+fx.xaxis.set_major_locator(MultipleLocator(1))
+fx.yaxis.set_major_locator(MultipleLocator(1))
+fx.xaxis.set_minor_locator(MultipleLocator(gridSizeA))
+fx.yaxis.set_minor_locator(MultipleLocator(gridSizeA))
+fx.legend()
+
+plt.show()
+
+
+# QUESTION 7
+
+robot_small_grid = Robot(gridSizeB, xMin, xMax, yMin, yMax)
+robot_small_grid.import_landmark_GT(filenameLandmarks)
+robot_small_grid.mark_occupied_cells()
+
+# CASE A
+robot_small_grid.a_star([2.45, -3.55], [0.95, -1.55])
+# Create plot
+fig, dx = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_small_grid.plot_landmark_pos(dx)
+robot_small_grid.plot_start_goal(dx)
+dx.set_aspect(1)
+dx.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case A (Question 5)')
+dx.xaxis.set_major_locator(MultipleLocator(1))
+dx.yaxis.set_major_locator(MultipleLocator(1))
+dx.xaxis.set_minor_locator(MultipleLocator(gridSizeB))
+dx.yaxis.set_minor_locator(MultipleLocator(gridSizeB))
+dx.legend()
+
+# CASE B
+robot_small_grid.reset_robot_path_data()
+robot_small_grid.a_star([4.5, 3.5], [4.5, -1.5])
+# Create plot
+fig, ex = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_small_grid.plot_landmark_pos(ex)
+robot_small_grid.plot_start_goal(ex)
+ex.set_aspect(1)
+ex.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case B (Question 5)')
+ex.xaxis.set_major_locator(MultipleLocator(1))
+ex.yaxis.set_major_locator(MultipleLocator(1))
+ex.xaxis.set_minor_locator(MultipleLocator(gridSizeB))
+ex.yaxis.set_minor_locator(MultipleLocator(gridSizeB))
+ex.legend()
+
+# CASE C
+robot_small_grid.reset_robot_path_data()
+robot_small_grid.a_star([-0.5, 5.5], [1.5, -3.5])
+# Create plot
+fig, fx = plt.subplots(figsize=(8, 8))
+plt.xlim(xMin, xMax)
+plt.ylim(yMin, yMax)
+plt.grid(which='minor')
+plt.grid(which='major')
+robot_small_grid.plot_landmark_pos(fx)
+robot_small_grid.plot_start_goal(fx)
+fx.set_aspect(1)
+fx.set(xlabel='X Position (m)', ylabel='Y Position (m)', title='Robot Path, Case C (Question 5)')
+fx.xaxis.set_major_locator(MultipleLocator(1))
+fx.yaxis.set_major_locator(MultipleLocator(1))
+fx.xaxis.set_minor_locator(MultipleLocator(gridSizeB))
+fx.yaxis.set_minor_locator(MultipleLocator(gridSizeB))
+fx.legend()
+
 plt.show()
